ce4c0bb8153f291c35c0652771a1f9dbf9b53e52
kmandalas <kmandalas@cognity.gr>
2024-11-27 23:21:13 +0200

    added testPartialUpdateViaPatch (expected to fail since Patch is not supported yet)

--- src/test/java/com/example/demo/audits/DemoAuditRepositoryIntegrationTest.java
+++ src/test/java/com/example/demo/audits/DemoAuditRepositoryIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.example.demo.audits;
 
+import com.azure.cosmos.models.CosmosPatchOperations;
+import com.azure.cosmos.models.PartitionKey;
 import com.example.demo.common.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +38,21 @@ class DemoAuditRepositoryIntegrationTest extends AbstractIntegrationTest {
         var auditsByTypes = demoAuditRepository.findAllByPrincipalAndTypeInAndTimestampGreaterThanEqualOrderByTimestampAsc("userA",
                 List.of("USER_ACTION_X", "USER_ACTION_Y"), Instant.now().minus(Duration.ofMinutes(1)));
         assertThat(auditsByTypes).hasSize(1);
+    }
+
+    // Expected to fail since Patch is not supported yet, src: https://learn.microsoft.com/en-us/azure/cosmos-db/emulator-linux#feature-support
+    @Test
+    void testPartialUpdateViaPatch() {
+        Map<String, Object> auditData = new HashMap<>();
+        auditData.put("request", "sample request");
+        auditData.put("response", "sample response");
+        auditService.publishEvent(new AuditEvent("userA", "USER_ACTION_X", auditData));
+
+        var audits = demoAuditRepository.findAll();
+        assertThat(audits).hasSize(1);
+
+        CosmosPatchOperations patchOperations = CosmosPatchOperations.create().replace("/type", "USER_ACTION_Z");
+        demoAuditRepository.save(audits.get(0).getId(), new PartitionKey(audits.get(0).getPrincipal()), DemoAudit.class, patchOperations);
     }
 
 }
